9f08d233b7cda0847c51c57f1c80a03da093a958
SaltContainer <fanmc34@gmail.com>
2023-09-07 14:35:33 -0400

    Remove Run Away change (it was working fine)

--- src/mod/features/ability_changes.cpp
+++ src/mod/features/ability_changes.cpp
@@ -5,18 +5,11 @@
 #include "externals/Dpr/Battle/Logic/BTL_CLIENT.h"
 #include "externals/Dpr/Battle/Logic/BTL_POKEPARAM.h"
 #include "externals/Dpr/Battle/Logic/CantEscapeCode.h"
-#include "externals/FlagWork.h"
 
 using namespace Dpr::Battle::Logic;
 
 HOOK_DEFINE_TRAMPOLINE(CheckForbidChangeEscapeCommon) {
     static Dpr::Battle::Logic::CantEscapeCode Callback(BTL_CLIENT *__this, BTL_POKEPARAM *procPoke, uint8_t *pokeID, uint16_t *tokuseiID) {
-        // Disallow escaping if can't escape flag is on
-        if (FlagWork::GetFlag(FlagWork_Flag::FLAG_CANT_ESCAPE))
-        {
-            return Dpr::Battle::Logic::CantEscapeCode::CANTESC_TOOSENBOU;
-        }
-
         // Nigeashi (Run Away) allows switch outs
         if (procPoke->GetValue(0x12) == array_index(ABILITIES, "Run Away"))
         {
