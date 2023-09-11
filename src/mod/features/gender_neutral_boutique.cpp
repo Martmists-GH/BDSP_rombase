753afb352a4778f8c29933fedb38c8debf08cafc
SaltContainer <fanmc34@gmail.com>
2023-09-11 01:00:21 -0400

    Re-indexed outfits

--- src/mod/features/gender_neutral_boutique.cpp
+++ src/mod/features/gender_neutral_boutique.cpp
@@ -6,8 +6,8 @@
 #include "externals/Dpr/UI/ShopBoutiqueItemItem.h"
 #include "externals/GameData/DataManager.h"
 
-void AddList_ShopBoutiqueItemItem_Param(System::Collections::Generic::List$$ShopBoutiqueItemItem_Param::Object *itemParams, int32_t dressId) {
-    Dpr::UI::ShopBoutiqueItemItem::Param::Object * item = Dpr::UI::ShopBoutiqueItemItem::Param::newInstance();
+void AddOutfit(System::Collections::Generic::List$$ShopBoutiqueItemItem_Param::Object* itemParams, int32_t dressId) {
+    Dpr::UI::ShopBoutiqueItemItem::Param::Object* item = Dpr::UI::ShopBoutiqueItemItem::Param::newInstance();
 
     item->fields.data = GameData::DataManager::GetBoutiqueShopData(dressId);
     item->fields.dressData = GameData::DataManager::GetCharacterDressData(dressId);
@@ -18,28 +18,24 @@ HOOK_DEFINE_REPLACE(Dpr_UI_ShopBoutiqueChange_SetupBoutiqueItemParams) {
     static int32_t Callback(System::Collections::Generic::List$$ShopBoutiqueItemItem_Param::Object *itemParams) {
         system_load_typeinfo(0x7cbb);
 
-        for (int32_t dressId = 0; dressId < OUTFIT_COUNT; dressId++) {
-            if (dressId == array_index(OUTFITS, "Contest Style")) {
-                // Don't add Contest Style just yet, missing animations
+        for (int32_t dressId = 0; dressId < OUTFIT_COUNT; dressId++)
+        {
+            if (dressId == array_index(OUTFITS, "Contest Style Masculine") ||
+                dressId == array_index(OUTFITS, "Contest Style Feminine") ||
+                dressId == array_index(OUTFITS, "Bicycle Style Masculine") ||
+                dressId == array_index(OUTFITS, "Bicycle Style Feminine") ||
+                dressId == array_index(OUTFITS, "Cyber Style 2.0 Feminine"))
+            {
+                // Don't add these outfits
                 continue;
             }
-
-            if (dressId == array_index(OUTFITS, "Everyday Style - Bike Outfit")) {
-                // Don't add bike outfit
-                continue;
+            else
+            {
+                AddOutfit(itemParams, dressId);
             }
-
-            if (dressId == array_index(OUTFITS, "New Cyber Style")) {
-                // Only add feminine new cyber style
-                AddList_ShopBoutiqueItemItem_Param(itemParams, (dressId) + 100); // Feminine Outfits
-                continue;
-            }
-
-            AddList_ShopBoutiqueItemItem_Param(itemParams, (dressId)); // Masculine Outfits
-            AddList_ShopBoutiqueItemItem_Param(itemParams, (dressId) + 100); // Feminine Outfits
         }
 
-        return 0; // Returning zero never hurt anyone right?
+        return 0;
     }
 };
 
