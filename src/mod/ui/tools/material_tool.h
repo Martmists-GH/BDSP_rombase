54c69cfa03562daaf52230ccd154b3033ba4941f
Samuel Lambert <fanmc34@gmail.com>
2023-09-27 16:59:19 -0400

    Log Renderers and Materials

--- src/mod/ui/tools/material_tool.h
+++ src/mod/ui/tools/material_tool.h
@@ -3,9 +3,39 @@
 #include "externals/BattleCharacterEntity.h"
 #include "externals/Dpr/UI/UIManager.h"
 #include "externals/FieldPokemonEntity.h"
+#include "externals/UnityEngine/_Object.h"
+#include "externals/UnityEngine/SkinnedMeshRenderer.h"
 #include "ui/base/collapsing_header.h"
 #include "ui/base/element.h"
 #include "ui/ui.h"
+
+void Debug_LogUIEntityRenderers() {
+    system_load_typeinfo(0x7deb);
+    system_load_typeinfo(0x1e84);
+    system_load_typeinfo(0x1e8d);
+
+    auto uiManager = Dpr::UI::UIManager::instance();
+    auto go = uiManager->fields._modelView->fields._modelParam->fields.gameObject;
+    auto unityObj = (UnityEngine::_Object::Object*)go;
+    Logger::log("GameObject %s\n", unityObj->GetName()->asCString().c_str());
+    ui_inspect(go->get_transform());
+
+    auto renderers = go->GetComponentsInChildren(false, UnityEngine::GameObject::Method$$SkinnedMeshRenderer$$GetComponentsInChildren);
+    if (renderers != nullptr)
+    {
+        for (uint64_t i=0; i<renderers->max_length; i++)
+        {
+            auto renderer = (UnityEngine::Renderer::Object*)renderers->m_Items[i];
+            Logger::log("  Renderer %d = %s\n", i, ((UnityEngine::_Object::Object*)renderer)->GetName()->asCString().c_str());
+            auto materials = renderer->get_materials();
+            for (uint64_t j=0; j<materials->max_length; j++)
+            {
+                auto material = materials->m_Items[j];
+                Logger::log("    Material %d = %s\n", j, ((UnityEngine::_Object::Object*)material)->GetName()->asCString().c_str());
+            }
+        }
+    }
+}
 
 namespace ui {
     ELEMENT(MaterialTool) {
@@ -13,51 +43,21 @@ namespace ui {
             auto header = CollapsingHeader::create([this](CollapsingHeader &_) {
                 _.label = "Material Tool";
 
-                _.Checkbox([](Checkbox &_) {
-                    _.label = "Overwrite material colors";
-                    _.enabled = false;
-                    _.onChange = [](bool value) {
-                        FlagWork::SetSysFlag(FlagWork_SysFlag::SYSFLAG_997, value);
-                    };
-                });
-
-                auto constantPicker = _.ColorEdit3([](ColorEdit3 &_) {
-                    _.label = "Field - Skin (Face)";
-                });
-
-                auto constantSdPicker = _.ColorEdit3([](ColorEdit3 &_) {
-                    _.label = "Field - Skin (Face)";
+                auto colorPicker = _.ColorEdit3([](ColorEdit3 &_) {
+                    _.label = "Color Value";
                 });
 
                 _.Button([](Button &_) {
-                    _.label = "Testing";
+                    _.label = "Log Renderers and Materials";
                     _.onClick = []() {
-                        auto uiManager = Dpr::UI::UIManager::instance();
-                        Logger::log("Getting entity type...\n");
-                        auto baseEntity = uiManager->fields._modelView->fields._modelParam->fields.baseEntity;
-                        Logger::log("BaseEntity %08X\n", baseEntity);
+                        Debug_LogUIEntityRenderers();
+                    };
+                });
 
-                        if (baseEntity != nullptr)
-                        {
-                            auto baseEntityKlass = (Il2CppClass*)baseEntity->klass;
-
-                            if (baseEntityKlass->isOfClass((Il2CppClass*)FieldPokemonEntity::getClass()))
-                            {
-                                Logger::log("FieldPokemonEntity!!!!\n");
-                            }
-
-                            if (baseEntityKlass->isOfClass((Il2CppClass*)BattleCharacterEntity::getClass()))
-                            {
-                                Logger::log("BattleCharacterEntity!!!!\n");
-                            }
-                        }
-
-                        /*auto methodPtr = *baseEntity->klass->vtable->_4_get_entityType.methodPtr;
-                        auto methodInfo = baseEntity->klass->vtable->_4_get_entityType.method;
-                        Logger::log("methodPtr %08X\n", methodPtr);
-                        Logger::log("methodInfo %08X\n", methodInfo);
-                        System::String::Object* type = methodPtr(baseEntity, methodInfo);
-                        Logger::log("Entity type is: %s\n", type->asCString().c_str());*/
+                _.Button([](Button &_) {
+                    _.label = "Log Properties of Selected Material";
+                    _.onClick = []() {
+                        Debug_LogUIEntityRenderers();
                     };
                 });
             });
