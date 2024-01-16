7cb2e568e566485081743dae81b849b9fe990558
SaltContainer <fanmc34@gmail.com>
2024-01-16 17:00:44 -0500

    Fix wrong feature checks for save features

--- src/mod/features/save_data.cpp
+++ src/mod/features/save_data.cpp
@@ -5,10 +5,10 @@
 #include "features/save_data/save_data.h"
 
 void exl_save_data_expansion_main() {
-    if (IsActivatedDebugFeature(array_index(SAVE_FEATURES, "Badge Expansion")))
+    if (IsActivatedSaveFeature(array_index(SAVE_FEATURES, "Badge Expansion")))
         exl_save_badges_expansion_main();
-    if (IsActivatedDebugFeature(array_index(SAVE_FEATURES, "Box Expansion")))
+    if (IsActivatedSaveFeature(array_index(SAVE_FEATURES, "Box Expansion")))
         exl_save_box_expansion_main();
-    if (IsActivatedDebugFeature(array_index(SAVE_FEATURES, "Dex Expansion")))
+    if (IsActivatedSaveFeature(array_index(SAVE_FEATURES, "Dex Expansion")))
         exl_save_dex_expansion_main();
 };
